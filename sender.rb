63d2683fd0fda0687ce17756082ede87d451bb66
David Bella <dbella1@gmail.com>
2013-10-24 16:07:34 -0400

    Cleaned up the HTML client; Worked with eventmachine a bit more to introduce a loop

--- sender.rb
+++ sender.rb
@@ -4,13 +4,15 @@ require 'eventmachine'
 EM.run do
   client = Faye::Client.new('http://localhost:9292/faye')
 
-  publication = client.publish('/msg', 'text' => 'Hello Faye!', "the time" => Time.now)
+  EM.add_periodic_timer(2) do
+    publication = client.publish('/msg', 'text' => 'Hello Faye!', "the time" => Time.now)
 
-  publication.callback do
-    EM.stop_event_loop
-  end
+    publication.callback do
+      # EM.stop_event_loop
+    end
 
-  publication.errback do
-    EM.stop_event_loop
+    publication.errback do
+      # EM.stop_event_loop
+    end
   end
 end
